cbf59d6548d60d3205ba55511f9d5c78c3a3b42c
Abhishek Prakash <1289prakash@gmail.com>
2023-03-31 03:57:34 +0530

    updated api and ux

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getChatResponse = async(message,product) => {
     try{
-        const resp = await axios.get(`https://demo.evmlabs.com/bot/api/chat?message=${message}&product=${product}`)
+        const resp = await axios.get(`http://1289prakash.com/api/chat?message=${message}&product=${product}`)
         if(resp.status === 200){
             return resp.data.data
         }
